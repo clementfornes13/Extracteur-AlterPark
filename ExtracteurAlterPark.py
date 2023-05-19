c745d2180ea00c192bba3375d21be87673b8122d
clementfornes13 <clement.fornes@viacesi.fr>
2023-05-19 14:02:30 +0200

    New Version

--- ExtracteurAlterPark.py
+++ ExtracteurAlterPark.py
@@ -1,114 +1,115 @@
-import PyPDF2
+from PyPDF2 import PdfReader
 from pdf2image import convert_from_path
-import cv2
-import pytesseract
-import openpyxl
-from PySimpleGUI import theme, Text, Button, Input, FileBrowse, FolderBrowse, Exit, Image, WIN_CLOSED, popup, Window
-from os import path, startfile, remove
-from re import findall
+from cv2 import imread, IMREAD_GRAYSCALE, rotate, ROTATE_180
+from pytesseract import image_to_string, pytesseract
+from openpyxl import Workbook
+from PySimpleGUI import theme, Text, Button, Input, FileBrowse, FolderBrowse, Exit, WIN_CLOSED, popup, Window
+from os import path, startfile, remove, add_dll_directory, path
+import re
+import sys
 
 class AlterParkExtractor:
-    def __init__(s):
-        
-
+    def __init__(self):
         theme('Material1')
-        s.IMG_PATH = path.join(path.dirname(__file__), 'images')
-        s.icon = path.join(s.IMG_PATH, 'Icone.ico')
-        s.logo = path.join(s.IMG_PATH, 'Logo TEA FOS.png')
-        
-    def run(s):
-        
-        
-        layout = [
-            [Text("Fichier PDF :"), Input(), FileBrowse('Parcourir',file_types=(("Fichiers PDF", "*.pdf"),))],
-            [Text("Destination de  l'extraction :"), Input(key='Destination'), FolderBrowse('Parcourir')],
-            [Button("Extraction"), Exit('Quitter')]]
-        
-        s.window = Window("Extracteur AlterPark", layout,icon=s.icon)
-        while True:
-            event, values = s.window.read()
-            if event == WIN_CLOSED or event == "Quitter": 
-                break
-            elif event == "Extraction": 
-                emplacement_pdf = values[0] 
-                if emplacement_pdf:
-                    pdf_file = open(emplacement_pdf, 'rb') 
-                    pdf_reader = PyPDF2.PdfReader(pdf_file)
-                    workbook = openpyxl.Workbook()
-                    sheet = workbook.active
-                    sheet.title = "AlterPark"
-                    sheet['A1'] = 'Numéro de réservation'
-                    sheet['B1'] = 'Immatriculation'
-                    sheet['C1'] = 'Date 1'
-                    sheet['D1'] = 'Date 2'
-                    sheet['E1'] = 'Montant total'
-                    sheet['F1'] = 'Apporteur'
-                    for page_num in range(len(pdf_reader.pages)):
-                        page = pdf_reader.pages[page_num]
-                        image = convert_from_path('C:/Users/clement.fornes/Desktop/ScriptsPython/Gitrepository/Extracteur-AlterPark/document.pdf', first_page=page_num+1, last_page=page_num+1)[0]
-                        image.save(f'page_{page_num+1}.jpg') #Converti en jpg et save
-                        img = cv2.imread(f'page_{page_num+1}.jpg', cv2.IMREAD_GRAYSCALE) #Lis l'image
-                        remove(f'page_{page_num+1}.jpg')
-                        test_reverse = [(1310, 150, 150, 100)] #Rectangle qui permet de savoir si c'est inversé ou non
-                        coordonnees = [
-                            (225, 70, 640, 80),  # Deplacement x , Deplacement y, Longueur, Largeur (Point en haut a gauche)
-                            (1120, 290, 400, 60),
-                            (190, 1180, 200, 50),
-                            (950, 1180, 200, 50),
-                            (1280, 1800, 240, 50),
-                            (1200, 1930, 300, 50)
-                        ]
-                        for x, y, w, h in test_reverse:
-                            test = img[y:y+h, x:x+w]
-                            pixel_img = (test == 50).sum()
-                            print(f"Il y a {pixel_img} pixels d'image dans le rectangle")
-                            p=0
-                            o=0
-                            if pixel_img >= 5:
-                                for x, y, w, h in coordonnees:
-                                    coord_rect = img[y:y+h, x:x+w]
-                                    #detection = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 1)
-                                    config = '--psm 3 --oem 3'
-                                    text = pytesseract.image_to_string(coord_rect, config=config)
-                                    print(f'Page {page_num+1}, Coordonnees : ({x}, {y}, {w}, {h}): {text}')
-                                    p=p+1
-                                    column_num = p  # assuming p=3 in this case
-                                    column_letter = openpyxl.utils.get_column_letter(column_num)
-                                    sheet[column_letter + str(page_num+1)] = text
-                                    #je dois save dans la colonne +1 a chaque fois. donc faire un c+= 
-                                    #same pour le else quand l'image est tournée
-                                    #chaque valeur en dessous max_row
-                                    #voir doc pour voir comment marquer sheet[column=row= etc..]
-                            else:
-                                img_rota = cv2.rotate(img, cv2.ROTATE_180)
-                                for x, y, w, h in coordonnees:
-                                    coord_rect = img_rota[y:y+h, x:x+w]
-                                    #detection = cv2.rectangle(img_rota, (x, y), (x+w, y+h), (0, 0, 255), 1)
-                                    config = '--psm 3 --oem 3'
-                                    text = pytesseract.image_to_string(coord_rect, config=config)
-                                    print(f'Page {page_num+1}, Coordonnees : ({x}, {y}, {w}, {h}): {text}')
-                                    o=o+1
-                                    column_num = o  # assuming p=3 in this case
-                                    column_letter = openpyxl.utils.get_column_letter(column_num)
-                                    sheet[column_letter + str(page_num+1)] = text
+        self.DATE_PATTERN = r"\d{2}/\d{2}/\d{4}"
+        self.IMMAT_PATTERN = r"[A-Z]{2}-[0-9]{3}-[A-Z]{2} | [A-Z]{2}[0-9]{3}[A-Z]{2}"
+        self.LIST_APPORTEUR = ['parkcloud', 'travelcar', 'ZENPARK', 'parkos', 'travelercar']
+        self.APPORTEUR_PATTERN = r"\b(" + "|".join(self.LIST_APPORTEUR) + r")\b"
 
-                        #cv2.imwrite(f'result_page_{page_num+1}.jpg', detection)
-                        #cv2.waitKey(0)
-                        #cv2.destroyAllWindows()
-                    file_name = "Extraction infos.xlsx".format(1)
-                    file_path = path.join(values['Destination'],file_name)
-                    i = 1
-                    while path.exists(file_path):
-                        i += 1
-                        file_name = "Extraction {}.xlsx".format(i)
-                        file_path = path.join(values['Destination'],file_name)
-                    workbook.save(file_path)
-                    popup("Fini!", f"Fichier enregistré ici : {file_path}")
-                    startfile(file_path)
+    def process_pdf(self, emplacement_pdf):
+        pdf_file = open(emplacement_pdf, 'rb')
+        pdf_reader = PdfReader(pdf_file)
+        workbook = Workbook()
+        sheet = workbook.active
+        sheet.title = "AlterPark"
+        sheet['A1'] = 'Numéro de réservation'
+        sheet['B1'] = 'Immatriculation'
+        sheet['C1'] = 'Date 1'
+        sheet['D1'] = 'Date 2'
+        sheet['E1'] = 'Montant total'
+        sheet['F1'] = 'Apporteur'
+        config = '--psm 3 --oem 3'
+        for page_num in range(len(pdf_reader.pages)):
+            image = convert_from_path(emplacement_pdf, first_page=page_num + 1, last_page=page_num + 1)[0]
+            image.save(f'page_{page_num + 1}.jpg')
+            img = imread(f'page_{page_num + 1}.jpg', IMREAD_GRAYSCALE)
+            remove(f'page_{page_num + 1}.jpg')
+            test_reverse = [(1280, 45, 320, 160)]
+            for x, y, w, h in test_reverse:
+                test = img[y:y + h, x:x + w]
+                pixel_img = (test == 50).sum()
+                if pixel_img >= 5:
+                    texte = image_to_string(img, config=config)
                 else:
-                 popup('Pas de fichier sélectionné, veuillez en sélectionner un !',title='Erreur',icon=s.icon)
-        s.window.close()
+                    img_rota = rotate(img, ROTATE_180)
+                    texte = image_to_string(img_rota, config=config)
+                resa = texte[8:15] if texte[8:15].startswith('A') else f'A{texte[8:15]}'
+                sheet[f'A{str(page_num + 2)}'] = resa.replace(" ", "").replace("O", "0")
+                sheet[f'B{str(page_num + 2)}'] = re.findall(self.DATE_PATTERN, texte)[0]
+                sheet[f'C{str(page_num + 2)}'] = re.findall(self.DATE_PATTERN, texte)[1]
+                if immat := re.findall(self.IMMAT_PATTERN, texte):
+                    sheet[f'D{str(page_num + 2)}'] = immat[0]
+                lignes = texte.split("\n")
+                for l in lignes:
+                    if "TOTAL" in l:
+                        montant_total = l.split("TOTAL", 1)[1].strip()
+                        if "€TTC" in montant_total:
+                            sheet[f'E{str(page_num + 2)}'] = montant_total
+                        else:
+                            sheet[f'E{str(page_num + 2)}'] = f"{montant_total} €TTC"
+                sheet[f'F{str(page_num + 2)}'] = re.findall(self.APPORTEUR_PATTERN, texte, flags=re.IGNORECASE)[0]
+        return workbook
+
+    def extraire_data(self, emplacement_pdf, destination):
+        if not emplacement_pdf:
+            popup('Pas de fichier sélectionné, veuillez en sélectionner un !', title='Erreur', icon=self.icon)
+            return
+        workbook = self.process_pdf(emplacement_pdf)
+        file_name = "Extraction AlterPark.xlsx".format(1)
+        file_path = path.join(destination, file_name)
+        i = 1
+        while path.exists(file_path):
+            i += 1
+            file_name = f"Extraction AlterPark {i}.xlsx"
+            file_path = path.join(destination, file_name)
+        workbook.save(file_path)
+        popup("Fini!", f"Fichier enregistré ici : {file_path}")
+        startfile(file_path)
+
+    def run(self):
+        layout = [
+            [Text("Fichier PDF :"), Input(), FileBrowse('Parcourir', file_types=(("Fichiers PDF", "*.pdf"),))],
+            [Text("Destination de l'extraction :"), Input(key='Destination'), FolderBrowse('Parcourir')],
+            [Button("Extraction"), Exit('Quitter')]
+        ]
+        self.window = Window("Extracteur AlterPark", layout)
+        while True:
+            event, values = self.window.read()
+            if event in [WIN_CLOSED, "Quitter"]:
+                break
+            elif event == "Extraction":
+                self.extraire_data(values[0], values['Destination'])
+        self.window.close()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = path.abspath(".")
+
+    return path.join(base_path, relative_path)
 
 if __name__ == '__main__':
+    if getattr(sys, 'frozen', False):
+        # Running as a bundled executable
+        bundle_dir = sys._MEIPASS
+    else:
+        # Running in the development environment
+        bundle_dir = path.dirname(path.abspath(__file__))
+    poppler_path = path.join(bundle_dir, 'poppler', 'bin')
+    pytesseract.tesseract_cmd = path.join(bundle_dir, 'tesseract', 'tesseract')
+    add_dll_directory(poppler_path)
     extracteur = AlterParkExtractor()
     extracteur.run()
